6a83acba744dc2b2ac36ba5ee33359e84351fe19
Xuanfeng Ling <lingxuanfeng@zh-jieli.com>
2024-07-22 10:03:11 +0800

    0001 bugfix

--- SDK/cpu/br28/tools/download.c
+++ SDK/cpu/br28/tools/download.c
@@ -101,16 +101,11 @@ set TONE_ZH_ENABLE=0
 set RCSP_EN=1
 #endif
 
-#ifdef CONIFG_EARPHONE_CASE
 #if TCFG_AUDIO_ANC_EAR_ADAPTIVE_EN
 copy anc_ext.bin download\earphone\ALIGN_DIR\.
 #else
 del download\earphone\ALIGN_DIR\anc_ext.bin
 #endif
 call download/earphone/download.bat
-#endif
 
-#ifdef CONIFG_SOUNDBOX_CASE
-call download/soundbox/download.bat
-#endif
 #endif
